1f596c5d5f1d32da4ad90b53e1a315613093a911
Dinesh Borude <79857147+dineshborude@users.noreply.github.com>
2021-11-09 20:25:27 +0530

    nutrition_brandwise
    Nutrition page data brandwise

---
+++ nutrition_brandwise.js
@@ -0,0 +1,469 @@
+var nutritionBrandData = [
+    {
+      title: "Axiom Karela Jamun Swaras (1000ml)",
+      src: "https://static.oxinis.com/healthmug/image/product/56346-1-200.jpg",
+      tagLine: "100% Natural Karela Jamun juice for overall wellbeing",
+      price: 300,
+      offprice: "",
+      discount: "",
+    },
+
+    {
+        title: "Axiom Sheesham Juice (500ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/56372-1-200.jpg",
+        tagLine: "It is an Ayurvedic juice which heals non-healing wounds, ulcers and is effective in acidity",
+        price: 180,
+        offprice: "",
+        discount: "",
+      },
+  
+      
+    {
+        title: "Axiom Triphla Aloevera Juice (1000ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/56382-1-200.jpg",
+        tagLine: "Axiom Triphla Aloevera Juice With goodness of Aloevera, this herbal juice is effective in blood pressure, constipation, acidity and treats various skin disorders",
+        price: 275,
+        offprice: 248,
+        discount: 10,
+      },
+  
+      
+    {
+        title: "Axiom Chhoti Duddhi Juice (500ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/56328-1-200.jpg",
+        tagLine: "Useful to manage the blood sugar level,loose motion,stomach ulcer, nausea & vomiting",
+        price: 300,
+        offprice: "",
+        discount: "",
+      },
+  
+      
+    {
+        title: "Axiom Karela Jamun Swaras (500ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/56345-1-200.jpg",
+        tagLine: "100% Natural Karela Jamun juice for overall wellbeing",
+        price: 160,
+        offprice: 144,
+        discount: 10,
+      },
+
+//   BigMuscles
+      
+    {
+        title: "Bigmuscles Nutrition Premium Gold Whey Protein Isolate & Concentrate Belgian Chocolate (1kg)",
+        src: "https://static.oxinis.com/healthmug/image/product/137209-4-200.jpg",
+        tagLine: "100% protein from whey isolate and whey concentrate, promotes muscle growth",
+        price: 2299,
+        offprice: 1299,
+        discount: 43,
+      },
+  
+      {
+        title: "Bigmuscles Nutrition Freak A Next Level Pre-Workout Formula Unflavored (180g)",
+        src: "https://static.oxinis.com/healthmug/image/product/137219-1-400.jpg",
+        tagLine: "Powerful pre-workout, helps you improve mental focus and go hard at the gym",
+        price: 1199,
+        offprice: 1079,
+        discount: 10,
+      },
+  
+      {
+        title: "Bigmuscles Nutrition Premium Gold Whey Protein Isolate & Concentrate Belgian Chocolate (2kg)",
+        src: "https://static.oxinis.com/healthmug/image/product/137211-1-400.jpg",
+        tagLine: "100% protein from whey isolate and whey concentrate, promotes muscle growth",
+        price: 4099,
+        offprice: 3074,
+        discount: 25,
+      },
+  
+      {
+        title: "Bigmuscles Nutrition Freak A Next Level Pre-Workout Formula Sydney Sunset (180g)",
+        src: "https://static.oxinis.com/healthmug/image/product/137220-1-400.jpg",
+        tagLine: "Powerful pre-workout, helps you improve mental focus and go hard at the gym",
+        price: 1199,
+        offprice: 1079,
+        discount: 10,
+      },
+  
+      {
+        title:"Bigmuscles Nutrition The Real Advanced Multivitamins (60tab)",
+        src:"https://static.oxinis.com/healthmug/image/product/137231-1-200.jpg",
+        tagLine: "Provides the essential vitamins to your muscles for stimulating optimal growth",
+        price: 599,
+        offprice: 389,
+        discount: 35,
+      },
+//   Yugantar
+      {
+        title:"Yugantar Gond (Gum) (200g)",
+        src: "https://static.oxinis.com/healthmug/image/product/131620-1-200.jpg",
+        tagLine: "Assists in keeping the body warm, Is nutrient dense, Rich in calcium",
+        price: 270,
+        offprice: 170,
+        discount: 37,
+      },
+  
+      {
+        title: "Yugantar Rock Sugar (Dhaga Mishri) (400g)",
+        src: "https://static.oxinis.com/healthmug/image/product/131641-1-400.jpg",
+        tagLine: "Useful in stomach ache, breathing problems, coughing",
+        price: 260,
+        offprice: 160,
+        discount: 38,
+      },
+  
+      {
+        title: "Yugantar Irani Methi Seeds (400g)",
+        src: "https://static.oxinis.com/healthmug/image/product/106992-4-400.jpg",
+        tagLine: "Reducing the body cholesterol, especially LDL or bad cholesterol, Lowers Blood Sugar.",
+        price: 600,
+        offprice: 510,
+        discount: 15,
+      },
+  
+      {
+        title:"Yugantar Poppy Seeds (Khaskhas) (200g)",
+        src: "https://static.oxinis.com/healthmug/image/product/137478-1-400.jpg",
+        tagLine: "Useful in promoting digestion and helps in various respiratory disorders",
+        price: 890,
+        offprice: 790,
+        discount: 11,
+      },
+  
+      {
+        title: "Yugantar Anjeer Premium Quality Dried Figs (100g)",
+        src: "https://static.oxinis.com/healthmug/image/product/128747-1-400.jpg",
+        tagLine: "Useful in prevention of constipation also useful for diebetic patients and for overall health.",
+        price: 333,
+        offprice: 185,
+        discount: 44,
+      },
+//   Healthvit
+      {
+        title: "Healthvit Vitamin B12 1500Mcg (60tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/139152-1-200.jpg",
+        tagLine: "",
+        price: 230,
+        offprice: 225,
+        discount: 2,
+      },
+  
+      {
+        title: "Healthvit Jointneed-500 Glucosamine Sulphate Tablets (60tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/72613-1-400.jpg",
+        tagLine: "The glucosamine in your body helps keep up the health of your cartilage.",
+        price: 250,
+        offprice: 170,
+        discount: 32,
+      },
+  
+      {
+        title: "Healthvit Vitamin K 55 Mcg (60caps)",
+        src: "https://static.oxinis.com/healthmug/image/product/139198-1-400.jpg",
+        tagLine: "",
+        price: 600,
+        offprice: 408,
+        discount: 32,
+      },
+  
+      {
+        title: "Healthvit Cenvitan Women 50+ Multivitamins And Multimineral 25 Nutrients (60tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/139269-2-400.jpg",
+        tagLine: "Cenvitan Women 50+ Multivitamins And Multimineral helps in hormonal regulation, energy and immunity booster",
+        price: 575,
+        offprice: 388,
+        discount: 33,
+      },
+  
+      {
+        title: "Healthvit Crampnil Calcium Magnesium L-Carnitine, With Vitamin E, B6, And D - 100 Tablets (100tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/139265-1-400.jpg",
+        tagLine: "",
+        price: 2500,
+        offprice: 1700,
+        discount: 32,
+      },
+
+//   Himalayan
+      {
+        title: "Himalayan Organics Biotin 10000Mcg Tablets (120tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/138000-1-200.jpg",
+        tagLine: "It facilitates healthier hair growth, glowing skin, and stronger nails",
+        price: 1299,
+        offprice: 599,
+        discount: 54,
+      },
+  
+      {
+        title: "Himalayan Organics Milk Thistle Extract Silymarin 800Mg Veg Capsules (120caps)",
+        src: "https://static.oxinis.com/healthmug/image/product/137937-1-400.jpg",
+        tagLine: "",
+        price: 1999,
+        offprice: 769,
+        discount: 62,
+      },
+  
+      {
+        title: "Himalayan Organics Plant Based D3 + K2 Veg Capsules (120caps)",
+        src: "https://static.oxinis.com/healthmug/image/product/137947-4-400.jpg",
+        tagLine: "",
+        price: 1275,
+        offprice: 599,
+        discount: 53,
+      },
+      {
+        title: "Himalayan Organics Organic Iron Supplement Veg Capsules (120caps)",
+        src: "https://static.oxinis.com/healthmug/image/product/137948-1-400.jpg",
+        tagLine: "",
+        price: 1095,
+        offprice: 599,
+        discount: 45,
+      },
+      
+      {
+        title: "Himalayan Organics Multivitamin Tablets (180tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/138026-1-400.jpg",
+        tagLine: "",
+        price: 955,
+        offprice: 549,
+        discount: 43,
+      },
+      
+    //   Basic Ayurvedaa
+      {
+        title: "Basic Ayurveda Brahmi Juice (500ml)",
+        src: "240",
+        tagLine: "Basic Ayurveda Brahmi Juice is an ideal juice which Improves Memory, Sharpens Thinking, Provides Good Mental Focus, & Relieves Stress",
+        price: 240,
+        offprice: 204,
+        discount: 15,
+      },
+      
+      {
+        title: "Basic Ayurveda Amla Juice (Goose Berry) (500ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/2425-1-400.jpg",
+        tagLine: "Basic Ayurveda Amla Juice (Goose Berry) is a Herbal juice that is rich in Vitamin C and helps in delaying Ageing Process & Promotes Hair Growth",
+        price: 120,
+        offprice: "",
+        discount: "",
+      },
+      
+      {
+        title: "Basic Ayurveda After Meal Hajma Churna (40g)",
+        src: "https://static.oxinis.com/healthmug/image/product/54140-2-400.jpg",
+        tagLine: "After Meal Hajma Churna for An ayurvedic digestive powder for all digestion related problems",
+        price: 60,
+        offprice: "",
+        discount: "",
+      },
+      
+      {
+        title: "Basic Ayurveda Apple Cider Vinegar (450ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/2443-2-400.jpg",
+        tagLine: "Basic Ayurveda Apple Cider Vinegar is an effective remedy which helps Reduce Weight, Improves Digestion, Lowers Cholesterol, Boosts Energy",
+        price: 225,
+        offprice: 224,
+        discount: 0,
+      },
+      
+      {
+        title: "Basic Ayurveda Ginger Juice (500ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/133429-1-400.jpg",
+        tagLine: "",
+        price: 225,
+        offprice: "",
+        discount: "",
+      },
+//   Baidynath Ayurveda
+      {
+        title: "Baidyanath Ayurved Ashwagandha Tablets (60tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/152938-1-200.jpg",
+        tagLine: "General Wellness Tablets act as an immunity booster and helps to handle stress and anxiety",
+        price: 150,
+        offprice: 145,
+        discount: 3,
+      },
+      
+      {
+        title: "Baidyanath Ayurved Pure Isabgol (Psyllium Husk) (200g, Pack of 2)",
+        src: "https://static.oxinis.com/healthmug/image/product/152528-1-400.jpg",
+        tagLine: "Rich source of dietary fibre for healthy digestive tract and provides relief from constipation (gluten-free)",
+        price: 420,
+        offprice: "",
+        discount: "",
+      },
+      
+      {
+        title: "Baidyanath Ayurved Honey (1kg)",
+        src: "https://static.oxinis.com/healthmug/image/product/154504-1-400.jpg",
+        tagLine: "Organic honey produced without chemicals or pesticides",
+        price: 400,
+        offprice: 350,
+        discount: 13,
+      },
+      
+      {
+        title: "Baidyanath Ayurved Karela Jamun Juice (1liter)",
+        src: "https://static.oxinis.com/healthmug/image/product/154502-1-400.jpg",
+        tagLine: "Organically made karela and jamun juice for healthy body and metabolism",
+        price: 300,
+        offprice: 270,
+        discount: 10,
+      },
+      
+      {
+        title: "Baidyanath Ayurved Triphala Juice (1liter)",
+        src: "https://static.oxinis.com/healthmug/image/product/154501-1-400.jpg",
+        tagLine: "Triphala Juice helps in constipation, flatulence, weight management & abdominal distension",
+        price: 250,
+        offprice: 230,
+        discount: 8,
+      },
+      
+//  Swadeshi   
+      {
+        title: "Swadeshi Ayurved Amla Sweet (900g)",
+        src: "https://static.oxinis.com/healthmug/image/product/151714-1-400.jpg",
+        tagLine: "Rich in Vitamin E and antioxidants ,aids in digestion, Increase red blood cells, eyesight improvement, Immunity booster",
+        price: 400,
+        offprice: 380,
+        discount: 5,
+      },
+      
+      {
+        title: "Swadeshi Ayurved Amla Murabba (1000g)",
+        src: "https://static.oxinis.com/healthmug/image/product/130963-1-400.jpg",
+        tagLine: "Ayurvedic eatable for digestive benefits",
+        price: 210,
+        offprice: "188",
+        discount: 10,
+      },
+      
+      {
+        title: "Swadeshi Ayurved Amla Sweet (900g, Pack of 2)",
+        src: "https://static.oxinis.com/healthmug/image/product/151734-1-400.jpg",
+        tagLine: "Rich in Vitamin E and antioxidants ,aids in digestion, Increase red blood cells, eyesight improvement, Immunity booster",
+        price: 800,
+        offprice: 750,
+        discount: 6,
+      },
+      
+      {
+        title: "Swadeshi Ayurved Bel Sweet (400g, Pack of 2)",
+        src: "https://static.oxinis.com/healthmug/image/product/151733-1-400.jpg",
+        tagLine: "Sweet Bel candy with health benefits.",
+        price: 400,
+        offprice: 380,
+        discount: 5,
+      },
+      
+      {
+        title: "Swadeshi Ayurved Amla Sweet (250g, Pack of 2)",
+        src: "https://static.oxinis.com/healthmug/image/product/151728-1-400.jpg",
+        tagLine: "Rich in Vitamin E and antioxidants ,aids in digestion, Increase red blood cells, eyesight improvement, Immunity booster",
+        price: 220,
+        offprice: 200,
+        discount: 9,
+      },
+
+// Shadani
+
+      {
+        title:"Shadani Coconut Sweet (200g)",
+        src: "https://static.oxinis.com/healthmug/image/product/129560-2-400.jpg",
+        tagLine: "Coconut sweets made from premium natural ingredients",
+        price: 130,
+        offprice: "",
+        discount: "",
+      },
+      
+      {
+        title: "Shadani Hazma Hazam Special (200g)",
+        src: "https://static.oxinis.com/healthmug/image/product/153139-1-400.jpg",
+        tagLine: "",
+        price: 120,
+        offprice: "",
+        discount: "",
+      },
+      
+      {
+        title: "Shadani Chennai Saunf (200g)",
+        src: "https://static.oxinis.com/healthmug/image/product/55890-2-400.jpg",
+        tagLine: "Mint Menthol coated saunf,Enjoyed by every age group",
+        price: 120,
+        offprice: "",
+        discount: "",
+      },
+      
+      {
+        title: "Shadani Plain Mix Saunf (215g)",
+        src: "https://static.oxinis.com/healthmug/image/product/45362-1-400.jpg",
+        tagLine: "Plain Mix Saunf for better digestion and fresh breath",
+        price: 120,
+        offprice: "",
+        discount: "",
+      },
+      
+      {
+        title: "Shadani Kala Khatta (250g)",
+        src: "https://static.oxinis.com/healthmug/image/product/55887-2-400.jpg",
+        tagLine: "",
+        price: 138,
+        offprice: "",
+        discount: "",
+      },
+      
+      {
+        title: "Sri Sri Tattva Karela - Jamun Juice (1000ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/53677-1-400.jpg",
+        tagLine: "Helps to lower increased blood sugar level, purifies blood, and improves digestive system",
+        price: 280,
+        offprice: 239,
+        discount: 15,
+      },
+      
+//  Sri Sri   
+      {
+        title: "Sri Sri Tattva Organic Virgin Coconut Oil (200ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/53681-5-400.jpg",
+        tagLine: "Oil that can help keep your body healthy and efficient.",
+        price: 300,
+        offprice: 270,
+        discount: 10,
+      },
+      
+      {
+        title: "Sri Sri Tattva Karela - Jamun Juice (1000ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/53677-2-400.jpg",
+        tagLine: "Helps to lower increased blood sugar level, purifies blood, and improves digestive system",
+        price: 280,
+        offprice: 239,
+        discount: 15,
+      },
+      
+      {
+        title: "Sri Sri Tattva Organic Jaggery (500g)",
+        src: "https://static.oxinis.com/healthmug/image/product/53683-3-400.jpg",
+        tagLine: "Organic cleanser to help you make extra fit and healthy",
+        price: 99,
+        offprice: "",
+        discount: "",
+      },
+      
+      {
+        title: "Sri Sri Tattva Organic Brown Sugar (1kg)",
+        src: "https://static.oxinis.com/healthmug/image/product/53682-3-400.jpg",
+        tagLine: "Organic Brown Sugar with lower calories as compared to refined white sugar",
+        price: 145,
+        offprice: "",
+        discount: "",
+      },
+      
+      {
+        title: "Sri Sri Tattva Amla Candy (400g)",
+        src: "https://static.oxinis.com/healthmug/image/product/53663-1-400.jpg",
+        tagLine: "Sri Sri Amla Candy for one and all",
+        price: 195,
+        offprice: "",
+        discount: ""
+      }]
