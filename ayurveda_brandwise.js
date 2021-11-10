526f82cd4fc23755279d5c7b38fa2367140b79a4
Dinesh Borude <79857147+dineshborude@users.noreply.github.com>
2021-11-10 17:02:42 +0530

    ayurveda data brandwise

---
+++ ayurveda_brandwise.js
@@ -0,0 +1,466 @@
+
+var ayurvedabrandData = [
+// Baidynath
+    {
+        title: "Baidyanath Bhringrajasava (450ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/6125-1-400.jpg",
+        tagLine: "Useful in seasonal cold and viral flu, reduces bloating and helps in digestive process",
+        price: 181,
+        offprice: "",
+        discount: "",
+    },
+
+
+    {
+        title: "Baidyanath Purnachandra Ras (Ordinary) (40tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/5876-2-400.jpg",
+        tagLine: "Acts well in Sexual Disorders Like Premature Ejaculations, Debility, Helps In Leucorrhea",
+        price: 143,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Baidyanath Chandraprabha Bati (80tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/5958-8-400.jpg",
+        tagLine: "For urinary tract disorders like (UTI), any bladder-related issues, muscle and joint pain and general weakness.",
+        price: 135,
+        offprice: 121,
+        discount: 10,
+    },
+
+     {
+        title: "Baidyanath Kanchnar Guggulu (80tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/6001-5-400.jpg",
+        tagLine: "For Thyroid Problems, Glandular Swellings, Skin Diseases, Sinuses, Fistula.",
+        price: 116,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Baidyanath Puspadhanwa Ras (5g)",
+        src: "https://static.oxinis.com/healthmug/image/product/5812-2-400.jpg",
+        tagLine: "For Lack of Vitality and Reproductive Disorders in Male and Female.",
+        price: 208,
+        offprice: "",
+        discount: "",
+    },
+
+//Dabur 
+    {
+        title: "Dabur Triphala Tablets (60tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/5718-2-400.jpg",
+        tagLine: "Ayurvedic powdered formulation for a healthy gut, overall well being etc",
+        price: 74,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Dabur Arjun Chhal Churna (100g)",
+        src: "https://static.oxinis.com/healthmug/image/product/128831-1-400.jpg",
+        tagLine: "It helps in keeping the heart muscles healthy and strong and may give relief from excessive thirst",
+        price: 80,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Dabur Mulethi (Yashtimadhu) Churna (100g)",
+        src: "https://static.oxinis.com/healthmug/image/product/129764-2-400.jpg",
+        tagLine: "Assists in managing various respiratory and digestive ailments",
+        price: 80,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Dabur Erand Tail (Castor Oil) (100ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/60559-1-400.jpg",
+        tagLine: "Erand taila(oil) or castor oil is very good for skin, hair and treating chronic constipation",
+        price: 108,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Dabur Jatyadi Tail (50ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/64778-1-400.jpg",
+        tagLine: "Dabur Jatyadi Tail is a herbal formulation that is used for healing in blisters, sinuses and non healing wounds",
+        price: 124,
+        offprice: "",
+        discount: "",
+    },
+
+// Unjha 
+
+    {
+        title: "Unjha Bilwasav (450ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/130156-3-400.jpg",
+        tagLine: "For helping with indigestion and dysentery",
+        price: "165",
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Unjha Pushpadhanwa Ras (80tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/54580-1-400.jpg",
+        tagLine: "Helps in both Male and Female Debilities, Restores Energy and Stamina",
+        price: 350,
+        offprice: 349,
+        discount: 0,
+    },
+
+    {
+        title: "Unjha Arogyavardhini Rasa (40tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/11740-1-400.jpg",
+        tagLine: "Fatty Liver, Jaundice, Hepatitis, Cirrhosis, Constipation, Indigestion, Skin Disorders",
+        price: 78,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Unjha Navratna Ras (Gold Coated) (30tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/11596-2-400.jpg",
+        tagLine: "Ayurvedic formulation that is highly beneficial and addresses the ailments like osteoporosis, kinds of bone degenerative diseases, and general debility",
+        price: 795,
+        offprice: "",
+        discount: "",
+    },
+    {
+        title: "Unjha Kanth Sudharak Vati (10g)",
+        src: "https://static.oxinis.com/healthmug/image/product/6881-2-400.jpg",
+        tagLine: "Effective In Sore Throat, Cough And Hoarseness, Impaired Taste In Mouth, Bad Breath",
+        price: 80,
+        offprice: "",
+        discount: "",
+    },
+
+// Himalaya
+
+    {
+        title: "Himalaya Ashwagandha Tablet (60tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/3263-2-400.jpg",
+        tagLine: "Remedy for nerves, mind and body's weakness, for body's defense mechanism.",
+        price: 165,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Himalaya Vrikshamla Tablet (60tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/72477-1-400.jpg",
+        tagLine: "",
+        price: 185,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Himalaya Cystone Tablet (60tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/3123-1-400.jpg",
+        tagLine: "Expels Renal Calculi, Control High Uric Acid, Urinary Track Infection (UTI)",
+        price: 135,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Himalaya Himcocid Suspension (Mint) (200ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/3139-1-400.jpg",
+        tagLine: "Relief from indigestion Acidity and Epigastric Pains, Peptic Ulcers, Acid Reflux, Heartburn",
+        price: 105,
+        offprice: 103,
+        discount: 2,
+    },
+
+    {
+        title: "Himalaya Liv 52 Tablet (100tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/3148-1-400.jpg",
+        tagLine: "Ayurveda for Liver Ailments, Loss of Appetite, Jaundice, Indigestion",
+        price: 120,
+        offprice: "",
+        discount: "",
+    },
+
+//Nagarjuna
+
+    {
+        title: "Nagarjun Varunadi Kashaya (200ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/6898-1-400.jpg",
+        tagLine: "Varanadi kashayam is an Ayurvedic liquid herbal medicine used in the treatment of headache, obesity and low digestion strength.",
+        price: 125,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Nagarjun Brihat Vasavaleh (400g)",
+        src: "https://static.oxinis.com/healthmug/image/product/51028-1-400.jpg",
+        tagLine: "For Productive Cough, Cold, Bronchitis, Asthma and Respiratory Problems.",
+        price: 255,
+        offprice: 254,
+        discount: 0,
+    },
+
+    {
+        title: "Nagarjun Ashmarihar Kashaya (200ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/12118-1-400.jpg",
+        tagLine: "Useful in Renal Calculi, with Pain and Difficult Urination.",
+        price: 125,
+        offprice: "",
+        discount: "",
+    },
+    {
+        title: "Nagarjun Kauch Pak (400g)",
+        src: "https://static.oxinis.com/healthmug/image/product/12114-1-400.jpg",
+        tagLine: "Male Tonic, Improve Sperm Thickness, Useful in Early Ejaculation and Weakness.",
+        price: 355,
+        offprice: 354,
+        discount: 0,
+    },
+
+    {
+        title: "Nagarjun Marichyadi Tailam (100ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/12096-1-400.jpghttps://static.oxinis.com/healthmug/image/product/12096-1-400.jpg",
+        tagLine: "Useful in Skin Diseases, Scabies, Psoriasis, Acne, Ulcers, Septic Wounds.",
+        price: 160,
+        offprice: "",
+        discount: "",
+    },
+
+// Dhootpapeshwar
+
+    {
+        title: "Dhootapapeshwar Punarnavasava (450ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/44447-1-400.jpg",
+        tagLine: "Punarnavasava is for liver disorders, jaundice, anaemia, fatty liver, gastrointestinal diseases and obesity.",
+        price: 149,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Dhootapapeshwar Ashwagandharishta (450ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/44298-1-400.jpg",
+        tagLine: "Ashwagandharishta is a herbal ayurvedic yonic for weakness, nervous debility, sleeplessness and improves memory",
+        price: 206,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Dhootapapeshwar Saraswatarishta (450ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/44476-1-400.jpg",
+        tagLine: "Indicated In memory weakness, insomina, stress, diseases related to brain",
+        price: 200,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Dhootapapeshwar Gokshuradi Guggul (60tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/44344-1-400.jpg",
+        tagLine: "Indicated to Relives pain, Gout, kidney stone, prostate related problems, urinary disorders.",
+        price: 157,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Dhootapapeshwar Abhayarishta (450ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/44277-1-400.jpg",
+        tagLine: "Useful in Constipation, Piles (Painful and Bleeding), Haemorrhoids, Fissures",
+        price: 162,
+        offprice: "",
+        discount: "",
+    },
+
+// Baidynath Nagpur 
+
+    {
+        title: "Baidyanath (Nagpur) Mahamanjisthadi Kadha (450ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/136230-1-400.jpg",
+        tagLine: "Useful in skin diseases, acne and pimples, inflammation, redness and skin rashes.",
+        price: 250,
+        offprice: 225,
+        discount: 10,
+    },
+
+    {
+        title: "Baidyanath (Nagpur) Abhayarishta (450ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/136167-1-400.jpg",
+        tagLine: "Useful for piles and Aids to relief constipation and other digestive problems",
+        price: 185,
+        offprice: 167,
+        discount: 10,
+    },
+    
+    {
+        title: "Baidyanath (Nagpur) Chandraprabha Bati (80tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/136205-1-400.jpg",
+        tagLine: "Useful in urinary problems as well as aids in menstural disorders and STDs",
+        price: 135,
+        offprice: 122,
+        discount: 10,
+    },
+
+    {
+        title: "Baidyanath (Nagpur) Maharasnadi Kadha (200ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/136234-1-400.jpg",
+        tagLine: "Used in rheumatism, sciatica, lumbago and difficulty in movemnet of joints, muscle stiffness",
+        price: 120,
+        offprice: 108,
+        discount: 10,
+    },
+
+    {
+        title: "Baidyanath (Nagpur) Amritarishta (220ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/136175-1-400.jpg",
+        tagLine: "Amritarishta is useful in chronic fever, digestion and maintaining blood sugar level",
+        price: 105,
+        offprice: 95,
+        discount: 10,
+    },
+// Basic Ayurveda 
+    {
+        title: "Basic Ayurveda Kamdev Churna (200g)",
+        src: "https://static.oxinis.com/healthmug/image/product/54301-2-400.jpg",
+        tagLine: "Basic Ayurveda Kamdev Churna is an effective Ayurvedic medicine for males which improves sexual health, vigor and stamina",
+        price: 260,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Basic Ayurveda Trifala Drishti Eye Drop (10ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/54531-2-400.jpg",
+        tagLine: "Basic Ayurveda Trifala Drishti Eye Drop is an Natural eye drops for dry eyes and for curing other eye-related ailments",
+        price: 62,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Basic Ayurveda Shankhpushpi Syrup (Brahmi Yukt) (450ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/54468-2-400.jpg",
+        tagLine: "Basic Ayurveda Shankhpushpi Syrup (Brahmi Yukt) is an excellent remedy for a sharp memory and enhancing brain power",
+        price: 255,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Basic Ayurveda Vidangasava (450ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/54555-2-400.jpg",
+        tagLine: "Vidangasava for treating parasitic worms in the body",
+        price: 144,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Basic Ayurveda Dashamool Kadha (450ml)",
+        src: "https://static.oxinis.com/healthmug/image/product/54245-2-400.jpg",
+        tagLine: "",
+        price: 180,
+        offprice: "",
+        discount: "",
+    },
+
+// Vyas
+    {
+        title: "Vyas Virya Sthambhan Vati (2g)",
+        src: "https://static.oxinis.com/healthmug/image/product/129763-1-400.jpg",
+        tagLine: "",
+        price: 325,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Vyas Kaunch Beej Churna (100g)",
+        src: "https://static.oxinis.com/healthmug/image/product/7003-1-400.jpg",
+        tagLine: "Male Tonic, Improve Sperm Thickness, Useful in Early Ejaculation and Weakness",
+        price: 90,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Vyas Kamdev Churna (100g)",
+        src: "https://static.oxinis.com/healthmug/image/product/111785-1-400.jpg",
+        tagLine: "",
+        price: 130,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Vyas Kutki Churna (100g",
+        src: "https://static.oxinis.com/healthmug/image/product/68242-2-400.jpg",
+        tagLine: "Detoxifies the Liver and Blood, Useful in Hepatitis C and Cirrhosis of the Liver",
+        price: 295,
+        offprice: "",
+        discount: "",
+    },
+
+     {
+        title: "Vyas Gokharu Churna (100g)",
+        src: "https://static.oxinis.com/healthmug/image/product/6996-2-400.jpg",
+        tagLine: "Useful in Urinary Track Infection, Frequent Urination with Burning",
+        price: 90,
+        offprice: "",
+        discount: "",
+    },
+//  Sri Sri Tatva 
+    {
+        title: "Sri Sri Tattva Ospo-C (60tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/53496-2-400.jpg",
+        tagLine: "Strengthens bones and improves overall bone health",
+        price: 180,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Sri Sri Tattva Brahma Rasayana (250g)",
+        src: "https://static.oxinis.com/healthmug/image/product/53556-2-400.jpg",
+        tagLine: "It improves digestion and relieves all the associated problems. It also helps in improving concentration.",
+        price: 130,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Sri Sri Tattva Oorja Tablet (60tab)",
+        src: "",
+        tagLine: "",
+        price: 150,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Sri Sri Tattva Vedanantaka Vati (60tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/53514-1-400.jpg",
+        tagLine: "",
+        price: 145,
+        offprice: "",
+        discount: "",
+    },
+
+    {
+        title: "Sri Sri Tattva Arjuna Tablet (60tab)",
+        src: "https://static.oxinis.com/healthmug/image/product/53521-2-400.jpg",
+        tagLine: "Arjuna Tablet Arjuna promotes a healthy cardiovascular system. The capsule helps to provide relief in angina, atherosclerosis",
+        price: 140,
+        offprice: "",
+        discount: "",
+    }
+]  
+
+   
